a561c7fd0ef395ad20bf68ae8ce79b4a4a345dd3
toshko89 <todor_petkov@mail.bg>
2021-11-06 15:20:22 +0200

    new css template

---
+++ src/components/Footer.js
@@ -0,0 +1,90 @@
+export default function Footer() {
+	return (
+		<footer class="footer">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-12 col-lg-4">
+						<div class="footer-logo">
+
+							<a class="navbar-brand" href="#">Kids Books</a>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-md-3 col-lg-2">
+						<div class="list-menu">
+
+							<h4>Abou Us</h4>
+
+							<ul class="list-unstyled">
+								<li><a href="#">About us</a></li>
+								<li><a href="#">Features item</a></li>
+								<li><a href="#">Live streaming</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-md-3 col-lg-2">
+						<div class="list-menu">
+
+							<h4>Abou Us</h4>
+
+							<ul class="list-unstyled">
+								<li><a href="#">About us</a></li>
+								<li><a href="#">Features item</a></li>
+								<li><a href="#">Live streaming</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-md-3 col-lg-2">
+						<div class="list-menu">
+
+							<h4>Support</h4>
+
+							<ul class="list-unstyled">
+								<li><a href="#">faq</a></li>
+								<li><a href="#">Editor help</a></li>
+								<li><a href="#">Contact us</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-md-3 col-lg-2">
+						<div class="list-menu">
+
+							<h4>Abou Us</h4>
+
+							<ul class="list-unstyled">
+								<li><a href="#">About us</a></li>
+								<li><a href="#">Features item</a></li>
+								<li><a href="#">Live streaming</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="copyrights">
+				<div class="container">
+					<p>&copy; Copyrights eStartup. All rights reserved.</p>
+					<div class="credits">
+						Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+					</div>
+				</div>
+			</div>
+
+		</footer>
+	);
+}
---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+export default function Header() {
+    return (
+        <header id="header" class="header header-hide">
+        <div class="container">
+    
+          <div id="logo" class="pull-left">
+            <h1><a href="#body" class="scrollto"><span>Kids</span>Share A Book</a></h1>
+          </div>
+    
+          <nav id="nav-menu-container">
+            <ul class="nav-menu">
+              <li class="menu-active"><a href="#home">Home</a></li>
+              <li><a href="#about-us">About</a></li>
+              <li><a href="#bookshelf">Bookshelf</a></li>
+              <li><a href="#my-books">My books</a></li>
+              <li><a href="#register">Register</a></li>
+              <li><a href="#login">Login</a></li>
+              <li><a href="#logout">Logout</a></li>
+              <li class="menu-has-children"><a href="">Drop Down</a>
+                <ul>
+                  <li><a href="#">Drop Down 1</a></li>
+                  <li><a href="#">Drop Down 3</a></li>
+                  <li><a href="#">Drop Down 4</a></li>
+                  <li><a href="#">Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+}
