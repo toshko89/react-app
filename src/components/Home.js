3f7ad31a3f067f63afd4a1c9b69a016b6189ca21
toshko89 <todor_petkov@mail.bg>
2021-11-11 10:16:22 +0200

    new Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
 
         <section id="hero" className="wow fadeIn">
             <div className="hero-container">
-                <h1>Welcome to eStartups</h1>
-                <h2>Elegant Bootstrap Template for Startups, Apps &amp; more...</h2>
-                <img src="img/children-reading-books.jpg" alt="Hero Imgs"/>
-                <a href="#get-started" className ="btn-get-started scrollto">Get Started</a>
+                <h1>Operation Book For Every Child</h1>
+                <h2>Platfom for Sharing Children's Books</h2>
+                <img src="img/children-reading-books.jpg" alt="Book Imgs"/>
+                <Link to="/bookshelf" className ="btn-get-started scrollto">Bookshelf</Link>
             </div>
         </section>
     );
