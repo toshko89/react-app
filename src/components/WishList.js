2218efe649054422c6a24f8b8d38645016aef16e
toshko89 <todor_petkov@mail.bg>
2021-12-25 15:01:26 +0200

    WishList card component added

--- src/components/WishList.js
+++ src/components/WishList.js
@@ -1,24 +1,31 @@
 import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router";
 import { UserContext } from "../context/userContext.js";
 import { getCurrentUserFromDB } from "../services/userService.js";
+import WishListCard from "./WishListCard.js";
 
 
 export default function WishList() {
 
-  const [userWishList, setUserWishList] = useState({});
   const user = useContext(UserContext);
+  const [userWishList, setUserWishList] = useState({});
+  const navigate = useNavigate();
+  const userData = sessionStorage.user || user.userId;
 
   useEffect(() => {
+    if (!userData) {
+      return navigate('/login');
+    }
     (async function fetchData() {
       try {
         const wishList = await getCurrentUserFromDB(user.userId);
-        console.log(wishList);
+        console.log(userWishList);
         setUserWishList(wishList);
       } catch (error) {
         console.log(error);
       }
     })();
-  }, [user.userId])
+  }, [user.userId, userData, navigate])
 
   return (
     <section id="pricing" className="padd-section text-center wow fadeInUp">
@@ -26,88 +33,14 @@ export default function WishList() {
       <div className="container">
         <div className="section-title text-center">
           <h2>My Wish List</h2>
-          <p className="separator">Integer cursus bibendum augue ac cursus .</p>
         </div>
       </div>
 
       <div className="container">
         <div className="row">
 
-          <div className="col-md-6 col-lg-3">
-            <div className="block-pricing">
-              <div className="table">
-                <h4>basic</h4>
-                <h2>$29</h2>
-                <ul className="list-unstyled">
-                  <li><b>4 GB</b> Ram</li>
-                  <li><b>7/24</b> Tech Support</li>
-                  <li><b>40 GB</b> SSD Cloud Storage</li>
-                  <li>Monthly Backups</li>
-                  <li>Palo Protection</li>
-                </ul>
-                <div className="table_btn">
-                  <a href="#" className="btn"><i className="fa fa-shopping-cart"></i> Buy Now</a>
-                </div>
-              </div>
-            </div>
-          </div>
+          {userWishList && <WishListCard />}
 
-          <div className="col-md-6 col-lg-3">
-            <div className="block-pricing">
-              <div className="table">
-                <h4>PERSONAL</h4>
-                <h2>$29</h2>
-                <ul className="list-unstyled">
-                  <li><b>4 GB</b> Ram</li>
-                  <li><b>7/24</b> Tech Support</li>
-                  <li><b>40 GB</b> SSD Cloud Storage</li>
-                  <li>Monthly Backups</li>
-                  <li>Palo Protection</li>
-                </ul>
-                <div className="table_btn">
-                  <button type="submit" className="btn btn-info px-3"><i className="fa fa-shopping-cart"></i>Order</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-6 col-lg-3">
-            <div className="block-pricing">
-              <div className="table">
-                <h4>BUSINESS</h4>
-                <h2>$29</h2>
-                <ul className="list-unstyled">
-                  <li><b>4 GB</b> Ram</li>
-                  <li><b>7/24</b> Tech Support</li>
-                  <li><b>40 GB</b> SSD Cloud Storage</li>
-                  <li>Monthly Backups</li>
-                  <li>Palo Protection</li>
-                </ul>
-                <div className="table_btn">
-                  <a href="#" className="btn"><i className="fa fa-shopping-cart"></i> Buy Now</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-6 col-lg-3">
-            <div className="block-pricing">
-              <div className="table">
-                <h4>profeesional</h4>
-                <h2>$29</h2>
-                <ul className="list-unstyled">
-                  <li><b>4 GB</b> Ram</li>
-                  <li><b>7/24</b> Tech Support</li>
-                  <li><b>40 GB</b> SSD Cloud Storage</li>
-                  <li>Monthly Backups</li>
-                  <li>Palo Protection</li>
-                </ul>
-                <div className="table_btn">
-                  <a href="#" className="btn"><i className="fa fa-shopping-cart"></i> Buy Now</a>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </section>
---
+++ src/components/WishListCard.js
@@ -0,0 +1,26 @@
+
+
+export default function WishListCard() {
+
+  return (
+    <div className="col-md-6 col-lg-3">
+      <div className="block-pricing">
+        <div className="table">
+          <h4>PERSONAL</h4>
+          <h2>$29</h2>
+          <ul className="list-unstyled">
+            <li><b>4 GB</b> Ram</li>
+            <li><b>7/24</b> Tech Support</li>
+            <li><b>40 GB</b> SSD Cloud Storage</li>
+            <li>Monthly Backups</li>
+            <li>Palo Protection</li>
+          </ul>
+          <div className="table_btn">
+            <button type="submit" className="btn btn-info px-3"><i className="fa fa-shopping-cart"></i>Order</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
